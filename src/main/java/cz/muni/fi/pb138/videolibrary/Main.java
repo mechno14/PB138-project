3b4a0fb95d79cc0a64002166a7982ccea3301e50
Denis Hambalek <xhambal1@fi.muni.cz>
2019-06-17 13:47:40 +0200

    db api

--- src/main/java/cz/muni/fi/pb138/videolibrary/Main.java
+++ src/main/java/cz/muni/fi/pb138/videolibrary/Main.java
@@ -1,8 +1,34 @@
 package cz.muni.fi.pb138.videolibrary;
 
+import java.util.List;
+
 public class Main {
     public static void main(String args[]) throws Exception {
-        NativeXMLDatabaseManager xml = new NativeXMLDatabaseManager();
-        xml.createCategory("TEST2");
+        NativeXMLDatabaseManager db = new NativeXMLDatabaseManager();
+        db.createCategory("TEST2");
+        db.deleteCategory("TEST2");
+        List<String> categories = db.findAllCategories();
+        for (String cat:
+        categories) {
+            System.out.println(cat);
+        }
+        System.out.println(db.findAllMediumsByCategory("Movies"));
+        db.deleteMediumFromCategory("6", "TEST");
+        db.addMediumToCategory("<medium id=\""+ db.getFirstFreeId() +"\"><mediumType>DVD</mediumType>\n" +
+                "                <name>HERK</name>\n" +
+                "                <length>90</length>\n" +
+                "                <actors>\n" +
+                "                    <actor>Mike Myers</actor>\n" +
+                "                </actors>\n" +
+                "                <status>available</status>\n" +
+                "                <genres>\n" +
+                "                    <genre>Animated</genre>\n" +
+                "                </genres>\n" +
+                "                <releaseYear>2001</releaseYear></medium>","TEST");
+
+
+        System.out.println(db.getFirstFreeId());
+
+        db.close();
     }
 }
