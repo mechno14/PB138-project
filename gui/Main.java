e48b3d29990531c2753802b48541f5944f088c2c
Natly Bebjakova <xbebjak@fi.muni.cz>
2019-06-18 16:56:18 +0200

    gui

---
+++ gui/Main.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Add_medium.fxml"));
+        primaryStage.setTitle("Video Library");
+        primaryStage.setScene(new Scene(root, 1080, 750));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
