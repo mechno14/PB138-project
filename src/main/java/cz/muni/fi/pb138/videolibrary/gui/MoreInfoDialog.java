145cf2fce7e5a061834d92fc70843505b6fba819
Natly Bebjakova <xbebjak@fi.muni.cz>
2019-06-21 13:16:58 +0200

    more_info_dialog

--- src/main/java/cz/muni/fi/pb138/videolibrary/gui/MoreInfoDialog.java
+++ src/main/java/cz/muni/fi/pb138/videolibrary/gui/MoreInfoDialog.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pb138.videolibrary.gui;
 
+import cz.muni.fi.pb138.videolibrary.entity.Genre;
 import cz.muni.fi.pb138.videolibrary.entity.Medium;
 
 import javax.swing.*;
@@ -8,23 +9,39 @@ import java.awt.event.ActionListener;
 
 public class MoreInfoDialog extends JDialog {
     private JPanel contentPane;
-    private JButton buttonOK;
     private JButton buttonCancel;
+    private JTextField textFieldName;
+    private JTextField textFieldGenres;
+    private JTextField textFieldYear;
+    private JTextField textFieldLength;
+    private JTextField textFieldActors;
+    private JTextField textFieldType;
 
     private Medium medium;
 
     public MoreInfoDialog(Medium medium) {
         setContentPane(contentPane);
         setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
+        getRootPane().setDefaultButton(buttonCancel);
 
         this.medium = medium;
 
-        buttonOK.addActionListener(new ActionListener() {
+        buttonCancel.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onOK();
             }
         });
+        textFieldName.setText(medium.getName());
+        textFieldLength.setText(Integer.toString(medium.getLength()));
+        textFieldYear.setText(Integer.toString(medium.getReleaseYear()));
+        textFieldType.setText(medium.getMediumType().name());
+        for (String actor : medium.getActors()) {
+            textFieldActors.setText(actor + ' ');
+        }
+        for (Genre genre : medium.getGenres()) {
+            textFieldGenres.setText(genre.name() + ' ');
+        }
+
     }
 
     private void onOK() {
