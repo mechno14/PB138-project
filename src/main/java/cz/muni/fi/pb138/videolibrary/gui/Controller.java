1782eb4e9938c3a6c0dc21f38fa8196d8cb1a811
Radoslav Cerhak <xcerhak@fi.muni.cz>
2019-06-21 16:05:50 +0200

    unnecessary files deleted

--- src/main/java/cz/muni/fi/pb138/videolibrary/gui/Controller.java
+++
@@ -1,4 +0,0 @@
-package cz.muni.fi.pb138.videolibrary.gui;
-
-public class Controller {
-}
--- src/main/java/cz/muni/fi/pb138/videolibrary/gui/gui.java
+++ src/main/java/cz/muni/fi/pb138/videolibrary/gui/gui.java
@@ -10,6 +10,7 @@ import org.apache.commons.io.FilenameUtils;
 import org.odftoolkit.simple.SpreadsheetDocument;
 import org.xmldb.api.base.XMLDBException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
@@ -18,6 +19,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
